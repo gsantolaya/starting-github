b884d664ee108397a632ce7a417769b386a68938
Guillermo Santolaya <guillermosantolaya.93@gmail.com>
2022-12-17 16:58:14 -0300

    initial commit

---
+++ js.js
@@ -0,0 +1,10 @@
+let edad = 33
+if (edad>=18) {
+    console.log("es mayor de edad");
+} else{
+    console.log("es menor de edad");
+}
+while (edad>12) {
+    console.log("la edad es de " + edad);
+    edad+1;
+}
